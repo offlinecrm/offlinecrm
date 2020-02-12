c139cb1ddbfcb2d02a16b188fbb9a4ce17b5a081
Gerzhan <nikolay.gerzhan@gmail.com>
2020-02-12 07:03:29 +0700

    fix lint warning

--- src/boot/i18n.ts
+++ src/boot/i18n.ts
@@ -9,7 +9,8 @@ const i18n = new VueI18n({
   fallbackLocale: 'en-us',
   messages
 });
-
+// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+// @ts-ignore
 export default ({ app }) => {
   // Set i18n instance on app
   app.i18n = i18n;
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -42,15 +42,15 @@
 </template>
 
 <script lang="ts">
-import EssentialLink from 'components/EssentialLink';
+import EssentialLink from 'components/EssentialLink.vue';
 
-import Vue from 'vue'
+import Vue from 'vue';
 
 export default Vue.extend({
   name: 'MainLayout',
 
   components: {
-    EssentialLink
+    EssentialLink,
   },
 
   data() {
@@ -61,40 +61,40 @@ export default Vue.extend({
           title: 'Docs',
           caption: 'quasar.dev',
           icon: 'school',
-          link: 'https://quasar.dev'
+          link: 'https://quasar.dev',
         },
         {
           title: 'Github',
           caption: 'github.com/quasarframework',
           icon: 'code',
-          link: 'https://github.com/quasarframework'
+          link: 'https://github.com/quasarframework',
         },
         {
           title: 'Discord Chat Channel',
           caption: 'chat.quasar.dev',
           icon: 'chat',
-          link: 'https://chat.quasar.dev'
+          link: 'https://chat.quasar.dev',
         },
         {
           title: 'Forum',
           caption: 'forum.quasar.dev',
           icon: 'record_voice_over',
-          link: 'https://forum.quasar.dev'
+          link: 'https://forum.quasar.dev',
         },
         {
           title: 'Twitter',
           caption: '@quasarframework',
           icon: 'rss_feed',
-          link: 'https://twitter.quasar.dev'
+          link: 'https://twitter.quasar.dev',
         },
         {
           title: 'Facebook',
           caption: '@QuasarFramework',
           icon: 'public',
-          link: 'https://facebook.quasar.dev'
-        }
-      ]
+          link: 'https://facebook.quasar.dev',
+        },
+      ],
     };
-  }
+  },
 });
 </script>
