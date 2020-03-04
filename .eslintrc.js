102aa896628ddfa94ea01428e154b0ca86363c4a
Gerzhan <nikolay.gerzhan@gmail.com>
2020-03-05 06:44:57 +0700

    update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,11 +7,11 @@ module.exports = {
   parserOptions: {
     parser: '@typescript-eslint/parser',
     sourceType: 'module',
-    project: './tsconfig.json'
+    project: './tsconfig.json',
   },
 
   env: {
-    browser: true
+    browser: true,
   },
   // Rules order is important, please avoid shuffling them
   extends: [
@@ -32,7 +32,7 @@ module.exports = {
     // https://github.com/typescript-eslint/typescript-eslint/tree/master/packages/eslint-plugin#usage-with-prettier
     'prettier',
     'prettier/@typescript-eslint',
-    'prettier/vue'
+    'prettier/vue',
   ],
 
   plugins: [
@@ -40,26 +40,26 @@ module.exports = {
     '@typescript-eslint',
     // Required to lint *.vue files
     // See https://eslint.vuejs.org/user-guide/#why-doesn-t-it-work-on-vue-file
-    'vue'
+    'vue',
     // Prettier has not been included as plugin to avoid performance impact
     // See https://github.com/typescript-eslint/typescript-eslint/issues/389#issuecomment-509292674
     // Add it as an extension
   ],
 
   globals: {
-    'ga': true, // Google Analytics
-    'cordova': true,
-    '__statics': true,
-    'process': true,
-    'Capacitor': true,
-    'chrome': true
+    ga: true, // Google Analytics
+    cordova: true,
+    __statics: true,
+    process: true,
+    Capacitor: true,
+    chrome: true,
   },
 
   // add your custom rules here
   rules: {
     'prefer-promise-reject-errors': 'off',
-    quotes: [ 'warn', 'single' ],
-    '@typescript-eslint/indent': [ 'warn', 2 ],
+    quotes: ['warn', 'single'],
+    '@typescript-eslint/indent': ['warn', 2],
 
     // allow console.log during development only
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
@@ -67,12 +67,14 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
 
     // Custom
-    'vue/component-name-in-template-casing': [ 'error', 'kebab-case' ],
+    'vue/component-name-in-template-casing': ['error', 'kebab-case'],
 
     // Correct typescript linting until at least 2.0.0 major release
     // See https://github.com/typescript-eslint/typescript-eslint/issues/501
     // See https://github.com/typescript-eslint/typescript-eslint/issues/493
     '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-member-accessibility': 'off'
-  }
-}
+    'brace-style': [2, 'stroustrup', {allowSingleLine: false}],
+    // disable the rule for all files
+    '@typescript-eslint/explicit-member-accessibility': 'off',
+  },
+};
